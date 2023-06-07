2a39a17f5d3f13d83485649bd6f4257d1ec6d71c
choisua120 <129369201+choisua120@users.noreply.github.com>
2023-06-07 16:29:36 +0900

    230607

---
+++ vogue/js/javascript.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+//메뉴버튼을 클릭했을때,nav와 section이 바뀌어라.
+    $('#wrap em').click(function(){
+        $('#wrap nav').addClass('on')
+        $('#wrap section').addClass('on');
+        //본인 스스로는 서서히 사라져라.
+        $(this).fadeOut(600)
+    })
+})
